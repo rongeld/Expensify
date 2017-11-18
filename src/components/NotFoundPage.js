611231dac3a4c4b7ba782eb613477c159eb5f22c
Andrii Zakrevskyi <andrew.zakrevskiy@gmail.com>
2017-11-18 15:53:49 +0200

    404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
   <div>
-    404 - <Link to="/">Go home</Link>
+  <iframe width="100%" height="600vh" src="https://www.youtube.com/embed/DLzxrzFCyOs?rel=0&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>
   </div>
 );
 
