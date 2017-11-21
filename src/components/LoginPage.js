fb9d85c33deb2f26253c22d620d083c606ffb429
Andrii Zakrevskyi <andrew.zakrevskiy@gmail.com>
2017-11-21 20:47:33 +0200

    facebook auth

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {startLogin} from '../actions/auth';
+import {startLoginGoogle, startLoginFacebook} from '../actions/auth';
 
-export const LoginPage = ({startLogin}) => {
+export const LoginPage = ({startLoginGoogle, startLoginFacebook}) => {
    return (
       <div>
-         <button onClick={startLogin}>Login</button>
+         <button onClick={startLoginGoogle}>Login with Google</button>
+         <button onClick={startLoginFacebook}>Login with Facebook</button>
       </div>
    )
 }
 
 const mapDispatchToProps = (dispatch) => ({
-   startLogin: () => dispatch(startLogin())
+   startLoginGoogle: () => dispatch(startLoginGoogle()),
+   startLoginFacebook: () => dispatch(startLoginFacebook())
 }) 
+
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
  
