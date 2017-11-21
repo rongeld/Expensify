327fb7b3c74d2684cc00a89bfa8f37323316f1a7
Andrii Zakrevskyi <andrew.zakrevskiy@gmail.com>
2017-11-21 23:01:48 +0200

    style login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -4,9 +4,11 @@ import {startLoginGoogle, startLoginFacebook} from '../actions/auth';
 
 export const LoginPage = ({startLoginGoogle, startLoginFacebook}) => {
    return (
-      <div>
-         <button onClick={startLoginGoogle}>Login with Google</button>
-         <button onClick={startLoginFacebook}>Login with Facebook</button>
+      <div className="login-page">
+         <div className="buttons__group">
+            <button onClick={startLoginGoogle} className="button-login button-login__google"><img src="/images/gbtn.png" alt="" className="button-login__google--img" /></button>
+            <button onClick={startLoginFacebook} className="button-login button-login__facebook"><img src="/images/fb-btn.png" alt="" className="button-login__fb--img" /></button>
+         </div>
       </div>
    )
 }
